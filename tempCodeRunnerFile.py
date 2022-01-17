5272675a2eb533ba85365dea66d78ee59a17eba9
boce1 <you@example.com>
2022-01-17 11:56:52 +0100

    ah shit here we go again

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-indow.fill(BLACK)
-        window.blit(game_over_message, (WIDTH // 2 - game_over_message.get_width() // 2,
-                                    HEIGHT // 2 - game_over_message.get_height() // 2))
-        pygame.display.update()
-        pygame.time.wait(1000)
-        menu = True
-        print("lol")
